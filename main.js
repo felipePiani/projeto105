f6c8fe592cee8dcad49847d183d74dd3890c3e28
felipePiani <111539507+felipePiani@users.noreply.github.com>
2023-02-23 18:35:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1imzBeQKx//model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1imzBeQKx/model.json',modelLoaded);
 
 function modelLoaded()
 {
